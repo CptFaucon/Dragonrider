2df8ba92abc0b71e5a18696c7908e80eb120300e
Noé <noe.emery@gmail.com>
2020-02-03 17:06:17 +0100

    Path manager + movements

---
+++ Assets/Scripts/PathManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    private Color rayColor = Color.white;
+    public List<Transform> pathTransforms = new List<Transform> ();
+    Transform[] theArray;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        theArray = GetComponentsInChildren<Transform> ();
+        pathTransforms.Clear();
+
+        foreach (Transform pathTransform in theArray)
+        {
+            if (pathTransform != this.transform)
+            {
+                pathTransforms.Add(pathTransform);
+            }
+        }
+
+        for (int i = 0; i < pathTransforms.Count; i++)
+        {
+            Vector3 position = pathTransforms[i].position;
+            if(i > 0)
+            {
+                Vector3 previous = pathTransforms[i - 1].position;
+                Gizmos.DrawLine (previous, position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMovement : MonoBehaviour
+{
+    public PathManager PathToFollow;
+
+    public int CurrentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 1.0f;
+    public float rotationSpeed;
+    public string pathName;
+
+    Vector3 lastPosition;
+    Vector3 currentPosition;
+
+    private void Start()
+    {
+        PathToFollow = GameObject.Find(pathName).GetComponent<PathManager> ();
+        lastPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(PathToFollow.pathTransforms[CurrentWayPointID].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, PathToFollow.pathTransforms[CurrentWayPointID].position, Time.deltaTime * speed);
+
+        var rotation = Quaternion.LookRotation(PathToFollow.pathTransforms[CurrentWayPointID].position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+
+        if(distance <= reachDistance)
+        {
+            CurrentWayPointID++;
+        }
+
+        if (CurrentWayPointID >= PathToFollow.pathTransforms.Count)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+ 
