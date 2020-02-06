64895934db61706c742c8223b51b6fbff6cfecc0
Thomas <rogulskith@gmail.com>
2020-02-06 18:46:42 +0100

    Fix CameraController

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     #region Variables
+    [SerializeField]
+    private GameObject body;
+
     [Header("   Movement Zone")]
     [SerializeField]
     private float limit = 4;
@@ -21,23 +24,20 @@ public class CameraController : MonoBehaviour
     [Range(0, 1)]
     [SerializeField]
     private float lerpSpeed = .05f;
-
-    private PlayerController pc;
+    
     #endregion
 
-
-    private void Awake()
-    {
-        pc = FindObjectOfType<PlayerController>();
-    }
+    
 
 
     private void FollowPlayer()
     {
         Vector3 position = transform.localPosition;
-        Vector2 desiredPosition = pc.gameObject.transform.localPosition;
+        Vector2 desiredPosition = body.transform.localPosition;
 
         desiredPosition.x = Mathf.Clamp(desiredPosition.x, -limit / 2, limit / 2);
+
+
 
         position = Vector3.Lerp(position, desiredPosition, lerpSpeed);
         position.z = offsetZ;
