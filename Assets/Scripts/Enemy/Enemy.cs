90dadeaa5fa33cb72b06534a673ce51ea04c1be7
CptFaucon <noe.emery@gmail.com>
2020-02-29 13:01:05 +0100

    Various Enemy parameter change

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,22 +11,8 @@ public class Enemy : MonoBehaviour
     private bool stayDurationEnded;
     private bool lastPath;
 
-    private List<Vector3> spawnPoints = new List<Vector3>();
+    private List<Vector3> spawnPoints;
     private Vector3 spawnPos;
-
-    [Header("Paramètres de vitesse")]
-    public float currentSpeed;
-    public float maxSpeed;
-    public float minSpeed;
-    public float accelerationAndDecelerationSpeed;
-
-    [Header("Paramètres de freinage")]
-    public float brakeDistance;
-    public float stopDuration;
-
-    [Header("Portées horizontales et verticales")]
-    public float HorizontalRange;
-    public float VerticalRange;
 
     [Header("Position des points de spawn")]
     public float upSpawn;
@@ -34,8 +20,22 @@ public class Enemy : MonoBehaviour
     public float rightSpawn;
     public float leftSpawn;
 
-    [Header("Durée durant laquelle l'ennemi reste à l'écran")]
+    [Header("Limites horizontales et verticales des déplacements")]
+    public float HorizontalRange;
+    public float VerticalRange;
+
+    [Header("Durée pendant laquelle l'ennemi reste à l'écran")]
     public float stayDuration;
+
+    [Header("Paramètres de vitesse")]
+    public float currentSpeed;
+    public float maxSpeed;
+    public float minSpeed;
+    public float accelerationAndDecelerationSpeed;
+
+    [Header("Paramètres de l'arrêt")]
+    public float brakeDistance;
+    public float stopDuration;
 
     private void Start()
     {
@@ -43,6 +43,8 @@ public class Enemy : MonoBehaviour
         target = transform.Find("enemyTarget");
 
         target.localPosition = new Vector3(Random.Range(-HorizontalRange, HorizontalRange), Random.Range(-VerticalRange, VerticalRange), 0);
+
+        spawnPoints = new List<Vector3>();
 
         spawnPoints.Add(new Vector3(0,upSpawn,0));
         spawnPoints.Add(new Vector3(0,downSpawn,0));
@@ -59,16 +61,16 @@ public class Enemy : MonoBehaviour
     {
         enemy.transform.localPosition = Vector3.MoveTowards(enemy.transform.localPosition, target.localPosition, currentSpeed * Time.deltaTime);
 
-        if (currentSpeed > maxSpeed) currentSpeed = maxSpeed;
-        if (currentSpeed < minSpeed && stopped == false) currentSpeed = minSpeed;
-
         if (Vector3.Distance(enemy.transform.position, target.position) > brakeDistance && stopped == false) currentSpeed += accelerationAndDecelerationSpeed * Time.deltaTime;
         if (Vector3.Distance(enemy.transform.position, target.position) < brakeDistance && stopped == false) currentSpeed -= accelerationAndDecelerationSpeed * Time.deltaTime;
+
+        if (currentSpeed > maxSpeed) currentSpeed = maxSpeed;
+        if (currentSpeed < minSpeed && stopped == false) currentSpeed = minSpeed;
 
         if (enemy.transform.localPosition == target.localPosition && stayDurationEnded == false && lastPath == false)
         {
             target.localPosition = new Vector3(Random.Range(-HorizontalRange, HorizontalRange), Random.Range(-VerticalRange, VerticalRange), 0);
-            StartCoroutine(Turning());
+            StartCoroutine(Stopping());
         }
 
         if (enemy.transform.localPosition == target.localPosition && stayDurationEnded == true && lastPath == false)
@@ -79,12 +81,12 @@ public class Enemy : MonoBehaviour
 
         if (enemy.transform.localPosition == target.localPosition && stayDurationEnded == true && lastPath == true)
         {
-            //Appeler fonction Thomas pour pulling (sortie)
+            //La désactivation de l'ennemi doit être appelée ici (pulling) et doit remplacer le SetActive
             gameObject.SetActive(false);
         }
     }
 
-    IEnumerator Turning()
+    IEnumerator Stopping()
     {
         stopped = true;
         currentSpeed = 0;
