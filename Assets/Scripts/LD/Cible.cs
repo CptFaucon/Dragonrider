5fd66facf3ad37597ef65ead724782219b007d3b
Thomas <rogulskith@gmail.com>
2020-03-07 22:18:11 +0100

    Fix Cible (to not have it disappear)

--- Assets/Scripts/LD/Cible.cs
+++ Assets/Scripts/LD/Cible.cs
@@ -4,7 +4,6 @@ public class Cible : Hittable
 {
     private ScoreManager sm;
     private ParticleSystem particle;
-    private Renderer r;
     public float scoreBonus;
 
     public override void OnHit()
@@ -13,11 +12,9 @@ public class Cible : Hittable
         {
             sm = FindObjectOfType<ScoreManager>();
             particle = transform.GetChild(0).GetComponent<ParticleSystem>();
-            r = GetComponent<MeshRenderer>();
         }
         if (!particle.isPlaying)
         {
-            r.enabled = false;
             particle.Play();
             sm.modifyScore(scoreBonus);
         }
