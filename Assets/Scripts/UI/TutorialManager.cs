0429b76b9317f3d5c64d078bc7e6426c197de36f
CptFaucon <noe.emery@gmail.com>
2020-03-17 17:10:53 +0100

    Tutorial text display

---
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour
+{
+    [Header("Le texte actuellement affiché")]
+    public int currentText;
+
+    [Header("Le tableau contenant tous les textes")]
+    public GameObject[] texts;
+
+    private Image BackgroundImage;
+    private Transform playerTransform;
+
+
+    private void Start()
+    {
+        texts = new GameObject[transform.childCount];
+        BackgroundImage = gameObject.GetComponent<Image>();
+        playerTransform = GameObject.Find("Player").transform;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            texts[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void DisplayNextText()
+    {
+        BackgroundImage.enabled = true;
+        if (currentText>=1) texts[currentText - 1].SetActive(false);
+        texts[currentText].SetActive(true);
+        currentText++;
+    }
+
+    IEnumerator TutorialTimer()
+    {
+        yield return null;
+    }
+}
