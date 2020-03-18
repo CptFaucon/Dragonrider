e036e5e7e0fd1c3e84c2d41afae38c03555071a2
Thomas <rogulskith@gmail.com>
2020-03-18 17:35:31 +0100

    Fix Cible

--- Assets/Scripts/LD/Cible.cs
+++ Assets/Scripts/LD/Cible.cs
@@ -1,21 +1,49 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Cible : Hittable
 {
     private ScoreManager sm;
-    private ParticleSystem particle;
+    private List<ParticleSystem> particle = new List<ParticleSystem>();
+    private Renderer rend;
+    private PlayerController pc;
 
     public override void OnHit()
     {
-        if (particle = null)
+        if (particle.Count == 0)
         {
             sm = FindObjectOfType<ScoreManager>();
-            particle = transform.GetChild(0).GetComponent<ParticleSystem>();
+            rend = GetComponent<Renderer>();
+            pc = FindObjectOfType<PlayerController>();
+            for (int i = 0; i < 3; i++)
+            {
+                particle.Add(transform.GetChild(0).transform.GetChild(i).GetComponent<ParticleSystem>());
+            }
         }
-        if (!particle.isPlaying)
+        if (!particle[0].isPlaying)
         {
-            particle.Play();
+            foreach (var item in particle)
+            {
+                item.Play();
+            }
             sm.modifyScore(scoreBonus);
+            rend.enabled = false;
+            StartCoroutine(ReAppearance());
         }
+    }
+
+    private IEnumerator ReAppearance()
+    {
+        int i = 0;
+        while (i < 2 / Time.fixedDeltaTime)
+        {
+            if (!pc.isOnPause)
+            {
+                i++;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+        rend.enabled = true;
     }
 }
