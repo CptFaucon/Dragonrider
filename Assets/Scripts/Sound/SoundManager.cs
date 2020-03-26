11c3624730fc5fef4fc2fa214b1f0c33886f1dd2
CptFaucon <noe.emery@gmail.com>
2020-03-27 00:18:39 +0100

    Wind background sound

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource enemyDie;
+    public AudioSource enemyEntry;
+    public AudioSource target;
+    public AudioSource wind;
+    public AudioSource menuNavig;
+    public AudioSource menuValid;
+    public AudioSource playerAttack;
+    public AudioSource playerCollide;
+    public AudioSource playerDodge;
+    public AudioSource playerWings;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Menu_Test")) StartCoroutine(WingDelay());
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Menu_Test"))
+        {
+            wind.Play();
+            wind.loop = true;
+        }
+    }
+
+    IEnumerator WingDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5015f);
+            playerWings.Play();
+            yield return new WaitForSeconds(0.5015f);
+        }
+    }
+}
