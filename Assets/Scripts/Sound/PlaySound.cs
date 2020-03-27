9ab8df875aeb54c104ef42d0b0be878b9e944b5b
CptFaucon <noe.emery@gmail.com>
2020-03-27 12:16:19 +0100

    Wing Sound fix

---
+++ Assets/Scripts/Sound/PlaySound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    private SoundManager sdm;
+
+    private void Start()
+    {
+        sdm = FindObjectOfType<SoundManager>();
+    }
+
+    public void CallSound()
+    {
+        sdm.WingSound();
+    }
+}
